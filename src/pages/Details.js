28c3541700be7113bf19fa531ea4f25e4c4668e1
Munsa1 <munsamibenge65@gmail.com>
2022-04-22 04:52:59 +0200

    Scooter details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import './details.css'
 
 function Details() {
   return (
     <Container>
       <div className="details-container">
         <div className="flex-direction">
+        <div className="d-flex">
+            <ul>
+                <button>Models</button>
+                <button>Lifestyle</button>
+                <button>Shop</button>
+                <button>Test Drive</button>
+            </ul>
+        </div>
         <div>
             <img className="thumbnail images rounded" src="https://images.carandbike.com/bike-images/large/bajaj/chetak/bajaj-chetak.jpg" alt="vacation house" />
           </div>
@@ -15,10 +24,9 @@ function Details() {
               <div>
               <h3 className="text-dark">Scooter Name</h3>
               <p className="description my-4">Description</p>
-              <div>
-              <p className="description">Scooter location info</p>
+              <p className="description">Scooter location</p>
               <p className="description">Scooter price</p>
-
+              <div>
               </div>
               <button variant="success" className="btn-success theme-btn mt-4 rounded" size="lg" type="button">Reservation</button>
 
